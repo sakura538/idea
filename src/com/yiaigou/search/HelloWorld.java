3b334fc4c0bd2f1672a53c982725770d99ab13e6
yijun <yijunyunnan@163.com>
2018-01-14 22:12:38 +0800

    Update HelloWorld.java
    add

--- src/com/yiaigou/search/HelloWorld.java
+++ src/com/yiaigou/search/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         int a = 10;
         System.out.println(a);
-        System.out.println("Hello1111111111111 World");
+        System.out.println("Hello1111111111111 edit by github World");
         System.out.println("Hello World");
         System.out.println(a);
     }
