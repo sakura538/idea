e6484f0f18ac19cc33b0a931d934f1bb31b5b290
yijun <yijunyunnan@163.com>
2018-01-14 20:25:31 +0800

    first idea

---
+++ src/com/yiaigou/search/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.yiaigou.search;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a);
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
+    }
+
+
+}
